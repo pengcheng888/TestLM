8b424070321081786caf4346e0b23c48dca00c3a
pengcheng888 <1033693766@qq.com>
2025-05-29 20:05:22 +0800

    jetson :: modify sh file

--- myinstall.sh
+++ myinstall.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-FolderPath= /home/ubuntu/workspace_aisys/LLM/
+FolderPath=/home/orin/workspace_aisys/TestLMcode
 export TEST_MODEL=/home/ubuntu/workspace_aisys/gguf_model/TinyLlama-1.1B-Chat-v1.0-F16.gguf
 
 #!
 #! 安装 operators
 #!
 function install_operators() {
-cd  "$FolderPath"
+echo "=========================================================================================="
+echo "==========================        install_operators     =================================="
+echo "=========================================================================================="
+
+sleep 1
+cd "$FolderPath" || { echo "无法进入目录: $FolderPath"; exit 1; }
+pwd
+
 git clone https://github.com/InfiniTensor/operators.git
 cd operators
 xmake f -v
@@ -20,7 +27,13 @@ xmake build && xmake install
 #! 安装 infer
 #!
 function install_infer(){
-cd  "$FolderPath"
+echo "=========================================================================================="
+echo "==========================         install_infer        =================================="
+echo "=========================================================================================="
+sleep 1
+cd "$FolderPath" || { echo "无法进入目录: $FolderPath"; exit 1; }
+pwd
+
 git clone https://github.com/InfiniTensor/infer.cc.git
 cd infer.cc
 xmake f --nv-gpu=true -cv
@@ -32,7 +45,14 @@ xmake && xmake install
 #! 安装 gguf
 #!
 function install_gguf(){
-cd  "$FolderPath"
+
+echo "=========================================================================================="
+echo "==========================         install_gguf         =================================="
+echo "=========================================================================================="
+sleep 1
+cd "$FolderPath" || { echo "无法进入目录: $FolderPath"; exit 1; }
+pwd
+
 git clone https://github.com/InfiniTensor/gguf.git
 cd gguf
 # cargo convert /home/ubuntu/workspace_aisys/gguf_model/TinyLlama-1.1B-Chat-v1.0-F16.gguf -x cast:linear:f16,norm:f32->merge-linear->sort --log trace
@@ -43,7 +63,14 @@ cd gguf
 #! 安装 InfiniLM
 #!
 function install_InfiniLM(){
-cd  "$FolderPath"
+echo "=========================================================================================="
+echo "==========================       install_InfiniLM       =================================="
+echo "=========================================================================================="
+
+sleep 1
+cd "$FolderPath" || { echo "无法进入目录: $FolderPath"; exit 1; }
+pwd
+
 git clone https://github.com/InfiniTensor/InfiniLM
 cd InfiniLM
 }
@@ -56,8 +83,14 @@ cd InfiniLM
 #! 在cpu上运行
 #!
 function run_cpu() {
-cd "$FolderPath"
+echo "=========================================================================================="
+echo "==========================          run_cpu             =================================="
+echo "=========================================================================================="
+sleep 1
+cd "$FolderPath" || { echo "无法进入目录: $FolderPath"; exit 1; }
 cd InfiniLM/
+pwd
+
 export DEVICES=1
 cargo test --release --package llama-cpu --lib -- infer::test_infer --exact --nocapture
 }
@@ -66,8 +99,14 @@ cargo test --release --package llama-cpu --lib -- infer::test_infer --exact --no
 #! 在gpu上运行
 #!
 function run_nvidia() {
-cd "$FolderPath"
+echo "=========================================================================================="
+echo "==========================       run_nvidia             =================================="
+echo "=========================================================================================="
+sleep 1
+cd "$FolderPath" || { echo "无法进入目录: $FolderPath"; exit 1; }
+
 cd InfiniLM/
+pwd
 export DEVICES=0
 cargo test --release --package llama-cuda --lib -- infer::test_infer --exact --nocapture
 }
@@ -79,18 +118,28 @@ cargo test --release --package llama-cuda --lib -- infer::test_infer --exact --n
 ##########################################################################
 ##########################################################################
 function install_test(){
-#install_operators
-#install_infer
-#install_gguf
-#install_InfiniLM
+echo "=========================================================================================="
+echo "==========================       install_test           =================================="
+echo "=========================================================================================="
+
+install_operators
+install_infer
+install_gguf
+install_InfiniLM
 pwd
 }
 
 function run_test(){
+echo "=========================================================================================="
+echo "==========================         run_test             =================================="
+echo "=========================================================================================="
 #run_cpu
-run_nvidia
+#run_nvidia
 pwd
 }
-
-
+##########################################################################
+##########################################################################
+##########################################################################
+##########################################################################
+install_test
 
